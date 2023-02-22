8025a8c8144e9589e77c4f06f9672264ddae65fc
Xavier <xavier.oyj1999@gmail.com>
2023-02-22 09:46:18 +0800

    Deleted residue file from MYSQL. Changed FirebaseInitialize to dynamic pathing so it works for everyone's project

---
+++ src/main/java/Entity/Bank.java
@@ -0,0 +1,12 @@
+package Clients;
+
+import java.sql.PreparedStatement;
+
+import Exceptions.UserNotFoundException;
+import Helpers.DBUtil;
+import Models.Account;
+import Models.User;
+import jdk.jshell.spi.ExecutionControl;
+
+public class Bank {
+}

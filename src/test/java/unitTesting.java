751c54450c44d979224083e6b929c1d8c0da9f30
unknown <magesdes@hotmail.com>
2023-03-19 17:03:55 +0800

    Unit Testing file format

---
+++ src/test/java/unitTesting.java
@@ -0,0 +1,23 @@
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import Services.AccountService;
+
+
+public class unitTesting {
+    
+    @Before
+    public void testBase(){
+
+    }
+
+    @Test
+    public void testAccount(){
+        
+    }
+
+}

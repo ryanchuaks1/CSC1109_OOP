f78b40c28753e86273aa0b5c7749ebada7d7e122
Devilsbats <magesdes@hotmail.com>
2023-03-24 15:05:34 +0800

    update to settings to combobox

--- src/main/java/com/rjdxbanking/rjdxbank/Controllers/SettingsController.java
+++ src/main/java/com/rjdxbanking/rjdxbank/Controllers/SettingsController.java
@@ -10,11 +10,13 @@ import com.rjdxbanking.rjdxbank.Services.AccountService;
 import com.rjdxbanking.rjdxbank.Services.UserService;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -32,11 +34,7 @@ import java.nio.file.Path;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-
-public class SettingsController implements Initializable{
-
-    @FXML
-    private TextField atmWithdrawalLimit;
+public class SettingsController implements Initializable {
 
     @FXML
     private Button btnChinese;
@@ -78,16 +76,19 @@ public class SettingsController implements Initializable{
     private ImageView iconPrimary;
 
     @FXML
-    private TextField internationalTransferLimit;
-
-    @FXML
     private Pane limitPane;
 
     @FXML
     private AnchorPane limitSettingsPane;
 
     @FXML
-    private TextField localTransferLimit;
+    private ComboBox<Double> atmWithdrawalLimit;
+
+    @FXML
+    private ComboBox<Double> internationalTransferLimit;
+
+    @FXML
+    private ComboBox<Double> localTransferLimit;
 
     @FXML
     private Label nameLabel;
@@ -147,7 +148,7 @@ public class SettingsController implements Initializable{
         UserService userService = new UserService();
         User currentUser = userService.getUserByUserId(SessionClient.getAccount().getUserId());
         nameLabel.setText(" " + currentUser.getFullName());
-        
+
         changePane.setVisible(false);
         verificationPane.setVisible(false);
         limitSettingsPane.setVisible(false);
@@ -156,9 +157,20 @@ public class SettingsController implements Initializable{
         addTextLimiter(oldPin, 6);
         addTextLimiter(newPin, 6);
 
-        localTransferLimit.setText(String.format("%.2f", SessionClient.getAccount().getLocalTransferLimit()));
-        atmWithdrawalLimit.setText(String.format("%.2f", SessionClient.getAccount().getATMWithdrawalLimit()));
-        internationalTransferLimit.setText(String.format("%.2f", SessionClient.getAccount().getInternationalTransferLimit()));
+        localTransferLimit.getItems().addAll(0.00, 500.00, 1000.00, 2000.00, 3000.00, 5000.00, 7000.00, 9000.00);
+        localTransferLimit.setValue(SessionClient.getAccount().getATMWithdrawalLimit());
+        atmWithdrawalLimit.getItems().addAll(0.00, 500.00, 1000.00, 2000.00, 3000.00, 5000.00, 7000.00, 9000.00);
+        atmWithdrawalLimit.setValue(SessionClient.getAccount().getATMWithdrawalLimit());
+        internationalTransferLimit.getItems().addAll(0.00, 500.00, 1000.00, 2000.00, 3000.00, 5000.00, 7000.00,
+                9000.00);
+        internationalTransferLimit.setValue(SessionClient.getAccount().getATMWithdrawalLimit());
+        // localTransferLimit.getItems(String.format("%.2f",
+        // essionClient.getAccount().getLocalTransferLimit()));
+        // atmWithdrawalLimit.setText(String.format("%.2f",
+        // SessionClient.getAccount().getATMWithdrawalLimit()));
+        // internationalTransferLimit
+        // .setText(String.format("%.2f",
+        // SessionClient.getAccount().getInternationalTransferLimit()));
     }
 
     @FXML
@@ -168,12 +180,11 @@ public class SettingsController implements Initializable{
         } else if (event.getSource() == changeLimitBtn) {
             changeLimitMethod();
         } else if (event.getSource() == verificationBtn) {
-            //verification
-            if(otpClient.verification(verificationOTP.getText())){
+            // verification
+            if (otpClient.verification(verificationOTP.getText())) {
                 verificationPane.setVisible(false);
                 changePane.setVisible(true);
-            }
-            else{
+            } else {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("VerificationOTP wrong");
                 alert.setHeaderText(null);
@@ -183,10 +194,10 @@ public class SettingsController implements Initializable{
         } else if (event.getSource() == reOTPBtn) {
             otpClient.phoneOTP(SessionClient.getAccount());
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("OTP resend");
-                alert.setHeaderText(null);
-                alert.setContentText("OTP resend to your phone");
-                alert.showAndWait();
+            alert.setTitle("OTP resend");
+            alert.setHeaderText(null);
+            alert.setContentText("OTP resend to your phone");
+            alert.showAndWait();
         } else if (event.getSource() == returnBackBtn) {
             verificationPane.setVisible(false);
             settingBox.setVisible(true);
@@ -196,18 +207,19 @@ public class SettingsController implements Initializable{
         }
     }
 
-    public void changePinMethod(){
+    public void changePinMethod() {
         String oldpin = oldPin.getText();
         String newpin = newPin.getText();
         String confirmNewin = newPin.getText();
 
         Account currentAcc = SessionClient.getAccount();
         AccountService accService = new AccountService();
-        //check if old is correct
+        // check if old is correct
         if (Password.check(oldpin, currentAcc.getPinNo()).withArgon2()) {
-            //check if new pin is same as old pin, if it is not the same perform change of pin
-            if(!Password.check(newpin, currentAcc.getPinNo()).withArgon2()){
-                if(newpin.equals(confirmNewin)){
+            // check if new pin is same as old pin, if it is not the same perform change of
+            // pin
+            if (!Password.check(newpin, currentAcc.getPinNo()).withArgon2()) {
+                if (newpin.equals(confirmNewin)) {
                     newpin = com.password4j.Password.hash(newpin).addRandomSalt().withArgon2().getResult();
                     accService.changePin(currentAcc, newpin);
                     Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -221,16 +233,14 @@ public class SettingsController implements Initializable{
                         // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
-                }
-                else{
+                } else {
                     Alert alert = new Alert(Alert.AlertType.ERROR);
                     alert.setTitle("New pin and confirm new pin are not the same");
                     alert.setHeaderText(null);
                     alert.setContentText("New pin and confirm New Pin are not the same");
                     alert.showAndWait();
                 }
-            }
-            else{
+            } else {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Pin cannot be the same as old pin");
                 alert.setHeaderText(null);
@@ -240,11 +250,14 @@ public class SettingsController implements Initializable{
         }
     }
 
-    public void changeLimitMethod(){
+    public void changeLimitMethod() {
         try {
-            aService.updateAccountLimits(SessionClient.getAccount(), "localTransferLimit", Double.parseDouble(localTransferLimit.getText()));
-            aService.updateAccountLimits(SessionClient.getAccount(), "internationalTransferLimit", Double.parseDouble(internationalTransferLimit.getText()));
-            aService.updateAccountLimits(SessionClient.getAccount(), "atmWithdrawalLimit", Double.parseDouble(atmWithdrawalLimit.getText()));
+            aService.updateAccountLimits(SessionClient.getAccount(), "localTransferLimit",
+                    localTransferLimit.getValue());
+            aService.updateAccountLimits(SessionClient.getAccount(), "internationalTransferLimit",
+                    internationalTransferLimit.getValue());
+            aService.updateAccountLimits(SessionClient.getAccount(), "atmWithdrawalLimit",
+                    atmWithdrawalLimit.getValue());
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Limits Updated");
             alert.setHeaderText(null);
@@ -258,18 +271,18 @@ public class SettingsController implements Initializable{
         } catch (Exception e) {
             System.out.println(e.getStackTrace());
         }
-        
+
     }
 
     public static void addTextLimiter(final PasswordField tf, final int maxLength) {
         tf.textProperty().addListener(new ChangeListener<String>() {
             @Override
-            public void changed(final ObservableValue<? extends String> ov, final String oldValue, final String newValue) {
+            public void changed(final ObservableValue<? extends String> ov, final String oldValue,
+                    final String newValue) {
                 if (tf.getText().length() > maxLength) {
                     String s = tf.getText().substring(0, maxLength);
                     tf.setText(s);
-                }
-                else if (!tf.getText().matches("\\d*") & tf.getText().length() < maxLength) {
+                } else if (!tf.getText().matches("\\d*") & tf.getText().length() < maxLength) {
                     tf.setText(newValue.replaceAll("[^\\d]", ""));
                 }
             }
